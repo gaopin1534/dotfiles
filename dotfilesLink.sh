df2df23919088ba1fb1b2355074fb0eacd24bed3
gaopin <kohei_takamatsu@r.recruit.co.jp>
2016-08-30 10:26:42 +0900

    fix some issues

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,4 +4,5 @@ ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/Brewfile ~/Brewfile
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -sf ~/dotfiles/.gitconfig.gaopin ~/.gitconfig.gaopin
 git config --global core.excludesfile ~/.gitignore_global
