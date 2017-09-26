c8ec555a286a53aba7391a2e0e4696d307639d7a
gaopin1534 <gaoxin.xixxix@gmail.com>
2017-09-26 13:35:30 +0900

    Revert "add dotfiles reflesh trackpad"
    This reverts commit b25c3f396283d5d603b9d3bdd7359cdc55aa7999.

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -6,5 +6,4 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/Brewfile ~/Brewfile
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/.gitconfig.gaopin ~/.gitconfig.gaopin
-ln -sf ~/dotfiles/reflesh_trackpad.sh ~/bin/reflesh_trackpad.sh
 git config --global core.excludesfile ~/.gitignore_global
